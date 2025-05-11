7873af488ae4f3380dc96e499fa4a75cc286179e
AIparsaChn <chrkhabyparsa@gmail.com>
2025-05-11 11:56:12 +0330

    threading a function called func for twice.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import time
+import threading
+
+
 def main():
-    print("Hello World!!")
+
+    def func(name):
+        print("Hello {name}.".format(name=name))
+        time.sleep(4)
+        print("Goodbye {name}.".format(name=name))
+
+
+    thread1 = threading.Thread(target=func, args=["Mohamad"])
+    thread2 = threading.Thread(target=func, args=["Mohamad"])
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
 
 if __name__ == "__main__":
     main()
